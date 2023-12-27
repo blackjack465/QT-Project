8dfc001cf9b322224a061be26e2ca3f65a3d9f6f
blackjack465 <abderahimouriachi@gmail.com>
2023-12-27 16:44:15 +0100

    commentaire3

--- gestion/admin.cpp
+++ gestion/admin.cpp
@@ -6,37 +6,38 @@
 
 using namespace std;
 
-Admin::Admin(const QSqlDatabase& database) {
+//Constructeur de la table Admin
+        Admin::Admin(const QSqlDatabase& database) {
 
-    if(database.isOpen())
-    {
-        qDebug () << "La base de donnee est ouvert ";
-    }else
-    {
-        qDebug () << "Erreur la base de donne n'a pas eter creer";
-    }
+            if(database.isOpen())
+            {
+                qDebug () << "La base de donnee est ouvert ";
+            }else
+            {
+                qDebug () << "Erreur la base de donne n'a pas eter creer";
+            }
 
-}
+        }
+//La methode pour creer la table Admin
+        void Admin::CreeTableAdmin()
+        {
+            QSqlQuery query;
 
-void Admin::CreeTableAdmin()
-{
-    QSqlQuery query;
-
-    if(query.exec("CREATE TABLE if not exists Admin (Admin_id primary key, Admin_name TEXT not null, Admin_gmail TEXT not null, Admin_password TEXT not null )"))
-    {
-        qDebug () << "La Creation de la table admin avec succes";
-    }else
-    {
-        qDebug () << "Erreur dans la creation de la table admin";
-    }
+            if(query.exec("CREATE TABLE if not exists Admin (Admin_id primary key, Admin_name TEXT not null, Admin_gmail TEXT not null, Admin_password TEXT not null )"))
+            {
+                qDebug () << "La Creation de la table admin avec succes";
+            }else
+            {
+                qDebug () << "Erreur dans la creation de la table admin";
+            }
 
 
-    if(query.exec("insert into admin  values (1,'Abderrahim','abderahimouriachi@gmail.com','123456789')"))
-    {
-        qDebug () << "Admin siyad Abderrahim cree avec succes";
-    }else
-    {
-        qDebug () << "Erreur dans l'insertion de l'admin";
-    }
+            if(query.exec("insert into admin  values (1,'Abderrahim','abderahimouriachi@gmail.com','123456789')"))
+            {
+                qDebug () << "Admin siyad Abderrahim cree avec succes";
+            }else
+            {
+                qDebug () << "Erreur dans l'insertion de l'admin";
+            }
 
-}
+        }
